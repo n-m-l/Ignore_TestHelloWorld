7d526b216ff50b5e67671d43e22ec870f7cfcf0b
n-m-l <nick_m_lee@hotmail.com>
2016-03-17 10:48:05 +0000

    Initial Setup

---
+++ HelloWorld/SayHello.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class SayHello
+    {
+        public List<IEmitter> Emitters { get; set; }
+
+        public SayHello(List<IEmitter> emitters)
+        {
+            Emitters = emitters;
+        }
+
+        public void Emit()
+        {
+            if (Emitters == null) return;
+            foreach (IEmitter emitter in Emitters) emitter.Emit("Hello World!");
+        }
+    }
+}
